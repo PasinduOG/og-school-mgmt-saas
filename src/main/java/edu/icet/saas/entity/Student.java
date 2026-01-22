a7eee54ab15cc2fe68315739c9428896cb9bc383
Pasindu Owa Gamage <pasinduogdev@gmail.com>
2026-01-22 11:00:19 +0530

    Added entites Student and Teacher

--- src/main/java/edu/icet/saas/entity/Student.java
+++ src/main/java/edu/icet/saas/entity/Student.java
@@ -1,4 +1,22 @@
 package edu.icet.saas.entity;
 
+import lombok.AllArgsConstructor;
+
+import java.util.UUID;
+
+@AllArgsConstructor
 public class Student {
+    private UUID id;
+    private String name;
+    private Integer age;
+    private String address;
+    private UUID cityId;
+    private UUID genderId;
+    private UUID classId;
+    private UUID clubId;
+    private UUID libraryId;
+    private UUID schoolId;
+    private UUID achievementId;
+    private UUID mediumId;
+    private UUID reportId;
 }
--- src/main/java/edu/icet/saas/entity/Teacher.java
+++ src/main/java/edu/icet/saas/entity/Teacher.java
@@ -1,4 +1,18 @@
 package edu.icet.saas.entity;
 
+import lombok.AllArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@AllArgsConstructor
 public class Teacher {
+    private UUID id;
+    private String name;
+    private String nic;
+    private String phone;
+    private String email;
+    private LocalDate dob;
+    private String address;
+    private Double salary;
 }
